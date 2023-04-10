9ac98be5f6c8bc9f12e6d989eb50e06bf534833c
saurav <sauravtajith@gmail.com>
2023-04-10 13:57:43 +0530

    trying again

---
+++ index.js
@@ -0,0 +1,64 @@
+require("dotenv/config");
+const { Client, IntentsBitField } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log("bot is online");
+});
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (message.channel.id !== process.env.CHANNEL_ID) return;
+  if (message.content.startsWith("!")) return;
+
+  let conversationLog = [
+    {
+      role: "system",
+      content: "chatbot at your service",
+    },
+  ];
+  conversationLog.push({
+    role: "user",
+    content: message.content,
+  });
+
+  await message.channel.sendTyping();
+
+  let prevMessages = await message.channel.messages.fetch({
+    limit: 15,
+  });
+  prevMessages.reverse();
+
+  prevMessages.forEach((msg) => {
+    if (message.content.startsWith("!")) return;
+    if (msg.author.id !== client.user.id && message.author.bot) return;
+    if (msg.author.id !== message.author.id) return;
+
+    conversationLog.push({
+      role: "user",
+      content: msg.content,
+    });
+  });
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: conversationLog,
+  });
+
+  message.reply(result.data.choices[0].message);
+});
+
+client.login(process.env.TOKEN);
